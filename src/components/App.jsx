4d1c29867d34e92946e25df740a87a5d7ea87991
CatalinaCap <c.cata1706@gmail.com>
2024-10-08 12:07:34 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import Profile from '../components/Profile';
+import user from '../components/user.json';
+import statsData from '../components/data.json';
+import Statistics from '../components/Statistics';
+import FriendList from './FriendList';
+import friends from '../components/friends.json';
+import transactions from '../components/transactions.json';
+import TransactionHistory from '../components/TransactionHistory';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="File types" stats={statsData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../components/Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+
+      <ul className={styles.statList}>
+        {stats.map((stat, index) => (
+          <li
+            key={stat.id}
+            className={`${styles.item} ${styles[`itemColor${index % 4}`]}`}
+          >
+            <span className={styles.label}>{stat.label}</span>
+            <span className={styles.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
